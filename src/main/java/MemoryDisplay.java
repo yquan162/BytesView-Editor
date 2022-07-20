70bacef16bbcc6fbc7356a897504e704254202bd
yquan162 <yquan162@gmail.com>
2022-07-19 21:20:56 -0500

    add ejectMemory function to MemoryDisplay so only one instance of MemoryDisplay needs to be used.

--- src/main/java/MemoryDisplay.java
+++ src/main/java/MemoryDisplay.java
@@ -9,6 +9,9 @@ public class MemoryDisplay {
     public void loadMemory(Memory mem){
         this.memory = mem;
     }
+    public void ejectMemory(){
+        this.memory = null;
+    }
     public void showMemory() throws MemoryAddressDoesNotExistException {
         System.out.println("Address       0 1 2 3 4 5 6 7");
         for(int i = 0; i < this.memory.size(); i++){
