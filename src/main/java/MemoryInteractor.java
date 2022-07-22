88fb4ad27cfedd4186d394956eb1fcddf63fb534
yquan162 <yquan162@gmail.com>
2022-07-21 23:34:45 -0500

    allow interactor to function independently.

--- src/main/java/MemoryInteractor.java
+++ src/main/java/MemoryInteractor.java
@@ -1,4 +1,6 @@
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 
@@ -54,8 +56,26 @@ public class MemoryInteractor {
             System.out.println("Memory of size "+ this.memory.size()+" flushed.");
         }
     }
-    public String sha256() throws MemoryAddressDoesNotExistException, NoSuchAlgorithmException, IOException {
-        MemoryDisplay mdisp = new MemoryDisplay(this.memory);
-        return mdisp.sha256();
+    public String sha256() throws NoSuchAlgorithmException, MemoryAddressDoesNotExistException, IOException {
+        if(this.memory == null){
+            System.out.println("This display instance does not have memory loaded!");
+            return new String();
+        }
+        final MessageDigest md = MessageDigest.getInstance("SHA-256");
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        final StringBuilder hexString = new StringBuilder();
+        int i = 0;
+        for(Map.Entry<Integer, Byte> entry:this.memory.map.entrySet()){
+            out.write(this.memory.getByte(i).toByteArray());
+            i++;
+        }
+        final byte[] hash = md.digest(out.toByteArray());
+        for (int j = 0; j < hash.length; j++) {
+            final String hex = Integer.toHexString(0xff & hash[j]);
+            if(hex.length() == 1)
+                hexString.append('0');
+            hexString.append(hex);
+        }
+        return hexString.toString();
     }
 }
